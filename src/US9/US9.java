4f25a8acb9bcbbf33adb85960febba3c044abe10
ZeynepBaglars <zeynepbaglars@gmail.com>
2023-09-08 23:54:00 +0300

    BaseDrivera Loggerları ekledim.Locaterları buldum.

--- src/US9/US9.java
+++ src/US9/US9.java
@@ -1,4 +1,21 @@
 package US9;
 
-public class US9 {
+import Utility.BaseDriver;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+
+
+public class US9 extends BaseDriver {
+    @Test
+    public void Test1(){
+        US9_POMElements elements=new US9_POMElements();
+        login();
+        elements.findPatientRecord.click();
+        elements.searchPatient.click();
+        elements.searchPatient.sendKeys("1001"+Keys.ENTER);
+
+
+
+        BekleVeKapat();
+    }
 }
--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 import java.time.Duration;
 
@@ -14,11 +17,16 @@ public class BaseDriver {
     public static WebDriver driver; //Singleton web driver
     public static WebDriverWait wait;
     static {   // public static driverBaslat() // bunun şartı extens olması ve basta yer alması
-        driver=new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
-       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+    ///Güncelelme uyarısı vermessin diye bu kısmı ekledim.
+
+      Logger logger= Logger.getLogger("");
+      logger.setLevel(Level.SEVERE);
+      driver=new ChromeDriver();
+      driver.manage().window().maximize();
+      driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+      driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+      wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
     }
     public static void BekleVeKapat(){
