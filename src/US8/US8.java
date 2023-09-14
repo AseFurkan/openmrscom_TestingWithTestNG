bdb7d32c0c14ec67db53d35c0846d0c3c78f3a2e
yoral8 <ysrorl@hotmail.com>
2023-09-14 15:58:15 +0300

    deneme başlangıcı

--- src/US8/US8.java
+++ src/US8/US8.java
@@ -1,4 +1,18 @@
 package US8;
 
-public class US8 {
+import Utility.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class US8 extends BaseDriver {
+    @Test
+    public void test1(){
+        login();
+        WebElement hastakaydı= driver.findElement
+                (By.xpath("//a[@id='coreapps-activeVisitsHomepageLink-coreapps-activeVisitsHomepageLink-extension']"));
+        WebElement sonsayfa= driver.findElement(By.xpath("//a[@id='patient-search-results-table_last']"));
+
+
+    }
 }
